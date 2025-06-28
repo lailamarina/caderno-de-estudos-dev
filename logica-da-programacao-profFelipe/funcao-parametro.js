776fc68a89fea10523b650408b025f3852d4bd97
Laila Marina <lailamarinadev@gmail.com>
2025-06-28 20:12:03 -0300

    funcao com parametro 2

--- logica-da-programacao-profFelipe/funcao-parametro.js
+++ logica-da-programacao-profFelipe/funcao-parametro.js
@@ -1,7 +1,11 @@
 //Aprendendo com Felipão da DIO
 
-torrar ("pão de forma")
+torrar ("pão de forma","Laila", 7.67)
+torrar ("pão integral","Marina" )
 
-function torrar(pao){
-    console.log("Torrada feita com + " + pao)
+function torrar(pao, nome = "Cliente não identificado", valor = "não registrado"){
+    console.log("Torrada feita com " + pao)
+    console.log("é um pedido de " + nome)
+    console.log("O valor total do pedido é R$ " + valor)
 }
+// Evite gambiarras, o valor opcional sempre ser o ultimo parametro.
