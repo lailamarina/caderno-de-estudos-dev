5b9fb02f1a858c9f475b8d4cece6718a1d075124
Laila Marina <lailamarinadev@gmail.com>
2025-07-18 18:54:34 -0300

    estudos 18-07

---
+++ logica-da-programacao-profFelipe/desafios/calculadora-partidas-ranqueadas.js
@@ -0,0 +1,33 @@
+//Calculadora de partidas Rankeadas proposta pelo Felipão da DIO
+
+calcularNivelRanked (85,10)
+let saldoDoJogador = calcularNivelRanked (85,10)
+
+console.log ("O herói tem de saldo " + saldoDoJogador.saldo + "está no nível de " + saldoDoJogador.nivel)
+
+function calcularNivelRanked(vitorias, derrotas) {
+    let saldo = vitorias-derrotas
+    let nivel = ""
+    if (vitorias < 10){
+        nivel = "Ferro"
+    } 
+    else if (vitorias >=10 && vitorias <=20){
+        nivel = "Bronze"
+    }
+    else if (vitorias > 20 && vitorias <=50) {
+        nivel = "Prata"
+    }
+    else if (vitorias > 51 && vitorias <=80){
+        nivel = "Ouro"
+    }
+    else if (vitorias >80 && vitorias <=90){
+        nivel = "Diamante"
+    }
+    else if (vitorias >90 && vitorias <=100){
+        nivel = "Lendário"
+    }
+    else if (vitorias >=101){
+        nivel = "Imortal"
+    }
+    return {saldo, nivel}
+}
---
+++ logica-da-programacao-profFelipe/retornar-funcao.js
@@ -0,0 +1,11 @@
+//quais situações utilizaria o retorno de uma função
+
+let resultado = soma(5, 10)
+//atribuir uma variável para uma função
+console.log("O resultado dessa função é " + resultado)
+function soma(numA, numB) {
+   let somatorio = numA + numB
+   return somatorio
+}
+// não tente retornar 2 valores, um só
+
